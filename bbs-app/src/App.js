5aa7d8295822ef3d43fdc3c74054b521f0c83d75
kirrard <35512725+kirrard@users.noreply.github.com>
2021-04-21 15:09:54 +0900

    [ADD] Register
    회원 정보 입력하고 확인 누를 시 리스트가 표시됨

--- bbs-app/src/App.js
+++ bbs-app/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React, { Component } from "react";
+import Register from "./components/Register";
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    information: [{
+      id: "",
+      password: "",
+      name: "",
+      phone: "",
+      email: "",
+      }
+    ]
+  };
 
-    </div>
-  );
-};
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({id:data.id, password: data.password, phone: data.phone, name: data.name, email:data.email})
+    })
+  }
+
+  render() {
+    const {information} =this.state;
+    return (
+      <div>
+        <Register onCreate={this.handleCreate}></Register>
+        {JSON.stringify(information)}
+      </div>
+    )}
+}
 
 export default App;
+//회원가입
+//목록 추가
+//삭제 ->없어지게
--- bbs-app/src/components/Register.js
+++ bbs-app/src/components/Register.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Register = () => {
+class Register extends Component {
+  state = {
+    id: "",
+    password: "",
+    name: "",
+    phone: "",
+    email: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onCreate(this.state);
+    this.setState({
+      id: "asdf",
+      password: "",
+      name: "",
+      phone: "",
+      email: "",
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
     return (
-        <div>
-
-        </div>
-    )
+      <form onSubmit={this.handleSubmit}>
+        아이디<input onChange={this.handleChange} name="id"></input>
+        <br></br>
+        비밀번호<input onChange={this.handleChange} name="password"></input>
+        <br></br>
+        전화번호<input onChange={this.handleChange} name="phone"></input>
+        <br></br>
+        이름<input onChange={this.handleChange} name="name"></input>
+        <br></br>
+        이메일<input onChange={this.handleChange} name="email"></input>
+        <br></br>
+        <button>확인</button> <button>취소</button>
+      </form>
+    );
+  }
 }
 
 export default Register;
